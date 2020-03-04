ae73f289f479750fa7c8cacf56f0e9d1898a08fa
lokin971115 <48173623+lokin971115@users.noreply.github.com>
2020-03-04 23:57:04 +0800

    changed

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
-	$('body').on('click','button',processForm);
+	$('#firstbutton').on('click',processForm);
+	$('body').on('click',"#secondbutton",processForm2);
 	$('body').on('click','a',replyclick);
 })
 
-function processForm() {
+function processForm2(){
 	let $new = $("<li><svg><circle></circle></svg><div><h5></h5><h6></h6><p></p><a href=\"#\" role=\"button\">Reply</a></li>");
+	var h5 = $(this).parent("form").find("#inputsubject").val();
+	var h6 = $(this).parent("form").find("#inputname").val();
+	var p = $(this).parent("form").find("#inputcomment").val();
+	var color = $(this).parent("form").find("input[name=inputcolor]:checked").val();
 	$new.addClass("media");
 	$new.find("div").addClass("media-body");
-	$new.find("h5").html($("#inputsubject").val());
-	$new.find("h6").html($("#inputname").val());
-	$new.find("p").html($("#inputcomment").val());
+	$new.find("h5").html(h5);
+	$new.find("h6").html(h6);
+	$new.find("p").html(p);
 	$new.find("svg").attr({
 		"height": 100,
 		"width": 100
@@ -18,15 +23,40 @@ function processForm() {
 		"cx": 50,
 		"cy": 50,
 		"r": 40,
-		"fill": $("input[name=inputcolor]:checked").val()
+		"fill": color
+	});
+	$(this).parent("form").parent("div").append($new);
+	$(this).parent("form").remove();
+}
+
+function processForm() {
+	var h5 = $("#firstform").find("#inputsubject").val();
+	var h6 = $("#firstform").find("#inputname").val();
+	var p = $("#firstform").find("#inputcomment").val();
+	var color = $("#firstform").find("input[name=inputcolor]:checked").val();
+	let $new = $("<li><svg><circle></circle></svg><div><h5></h5><h6></h6><p></p><a href=\"#\" role=\"button\">Reply</a></li>");
+	$new.addClass("media");
+	$new.find("div").addClass("media-body");
+	$new.find("h5").html(h5);
+	$new.find("h6").html(h6);
+	$new.find("p").html(p);
+	$new.find("svg").attr({
+		"height": 100,
+		"width": 100
+	});
+	$new.find("circle").attr({
+		"cx": 50,
+		"cy": 50,
+		"r": 40,
+		"fill": color
 	});
 	$("#comments").append($new);
-	$("form")[0].reset();
-	console.log(this);
+	$("#firstform")[0].reset();
 }
 
 function replyclick(){
-	var htmlelement=$("form").html();
-	$(this).parent("div").append(htmlelement);
-	console.log(this);
+	var $htmlelement=$("#firstform").clone();
+	$htmlelement.attr("id","secondform");
+	$htmlelement.find("button").attr("id","secondbutton");
+	$(this).parent("div").append($htmlelement);
 }
